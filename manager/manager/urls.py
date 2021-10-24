d5e52c7004b046951c52a242a6590418b745d6ef
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-24 11:09:47 -0400

    add wakemydyno

--- manager/manager/urls.py
+++ manager/manager/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def empty(request):
+    return HttpResponse('wakemydyno', content_type="text/plain")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('wakemydyno.txt', empty),
     path('api/authentication/', include('authentication.urls')),
     path('api/goals/', include('goals.urls')),
     path('api/schedule/', include('schedule.urls')),
